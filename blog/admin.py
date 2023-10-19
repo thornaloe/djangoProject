6e646f4a0e7474cf9ba999d64f00c5dc7d082188
YuryKhokhloff <146858860+YuryKhokhloff@users.noreply.github.com>
2023-10-19 20:01:17 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -7,3 +7,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author']
     search_fields = ['title']
     list_select_related = ['author']
+
+from django.apps import AppConfig
+
+
+class BlogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blogs'
