3784ccf5bbf208da16553df0719b95b84d872b17
YuryKhokhloff <146858860+YuryKhokhloff@users.noreply.github.com>
2023-10-19 20:56:22 +0300

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,12 +76,6 @@ WSGI_APPLICATION = 'postblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
